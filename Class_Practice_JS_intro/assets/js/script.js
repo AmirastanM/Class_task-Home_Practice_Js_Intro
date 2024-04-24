54f2f884380a68ce838187bed9b623e894669555
AmirastanM <amirastanshm@code.edu.az>
2024-04-24 22:21:53 +0400

    added tasks

---
+++ Class_Practice_JS_intro/assets/js/script.js
@@ -0,0 +1,163 @@
+"use strict";
+
+
+// var address = "Ehmedli";
+
+// console.log(address)
+
+// var address = "gence";
+
+// console.log(address)
+
+// let phone = "12312413412";
+
+// let phone = "12312413412";
+
+// const email = "test@gmail.com";
+
+// email = "test23232";
+
+// let a = 5;
+// let b = 10;
+// let result = a + b;
+// a++;
+// console.log(a/0)
+
+
+// let isMarried = true;
+
+// let number = 200;
+// if (isMarried == true){
+//     console.log("evlidir");
+
+// }else{
+//     console.log("Subaydir");
+// }
+
+// if (number > 50 && number < 150){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+
+// let weekDay = "Wednesday";
+
+// switch(weekDay){
+//     case "Tuesday":
+//         console.log("Tuesday")
+//         break;
+//     case "Wednesday":
+//         console.log("Wednesday")
+//         break;
+//     default:
+//         console.log("not selected")
+//         break;
+// }
+
+
+// alert("welcome to group")
+
+// let result = confirm("Press  a button");
+// console.log(result)
+
+// let person = prompt("Please enter your name", "Harry Poter");
+
+// if (person == null){
+//     console.log("Operation canseled")
+// }else{
+//     console.log(person)
+// }
+
+
+// let a = 5;
+// while(a<8){
+//     console.log(a)
+//     a++;
+// }
+
+// for (let i = 0; i < 50; i++){
+//     console.log(i)
+// }
+
+
+// let a = 5;
+// let b = a**2;
+// console.log(b)
+
+
+// let num1 = 100;
+
+// let num2 = "100";
+
+// console.log(num1 == num2)
+// let isActive = true;
+// console.log(typeof isActive == 'boolean')
+
+
+// let datas = [1, 2, 3, "salam", true];
+
+// console.log(datas[3])
+
+// for (let i = 0; i < datas.length; i++) {
+//     if (!isNaN(datas[i]) || typeof (datas[i]) != 'boolean') {
+//         console.log(datas[i])
+//     }
+// }
+
+// for (let i = 0; i < datas.length; i++) {
+//     if (typeof (datas[i]) == 'number') {
+//         console.log(datas[i])
+//     }
+// }
+
+// let elem = 50;
+
+// let result = elem > 100 ? "yes" : "no";
+
+// console.log(result)
+
+
+// let b;
+// let a = null;
+
+
+// let numbers = [30, 6, 8, 9, 15, 11];
+
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+
+// console.log(sum);
+
+
+// let isTrue = true;
+
+// if (isTrue) {
+//     var name = "Feriyye";
+// }
+
+
+// console.log(name)
+
+
+// const data = "Product1";
+
+// for (let i = 0; i < products.length; i++) {
+//     console.log(products[i])
+// }
+
+
+// const name = "test";
+// name = "test2";
+
+
+// const datas = [1, 2, 3, 4, 5];
+
+// const datas2 = [2, 2, 3, 1, 4, 5, 4];
+
+// datas = datas2
+
+// datas[2] = 100;
---
+++ Class_tasks/assets/js/script.js
@@ -0,0 +1,120 @@
+"use strict";
+
+// let n = 10;
+
+// if (n % 3 && n % 7) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+
+
+// let a = 12;
+// let b = 8;
+
+
+// if(a%2 == 0 && b%2 == 0){
+//     let result = a*b;
+//     console.log(result);
+
+// }else{
+//     console.log("numbers is not even");
+// }
+
+
+// let a = 12;
+// let b = 8;
+
+// let count = 0;
+
+// for (let i = a+1; i < b; i++){
+//     if(i%2){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+// let a = 12;
+// let b = 8;
+
+// let sum = 0;
+
+// for (let i = a + 1; i < b; i++) {
+//     if (i % 2) {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+
+// let a = 12;
+// let b = 8;
+
+// let sum = 0;
+
+// for (let i = a + 1; i < b; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i)
+//     }
+// }
+// console.log(sum);
+
+
+
+// let numbers = [1, 5, 2, 6, 3, 9];
+
+// let count = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (nubmers[i] % 2 == 0) {
+//         count++;
+//     }
+
+// }
+// console.log(count);
+
+
+
+
+// let a = 6;
+
+// let count = 0;
+
+// for (let i = a; i >= a; i--) {
+//     if (a % 2 == 0) {
+//         count++;
+//     }
+//     if (i > 2) {
+//         console.log("murekkeb");
+//     } else {
+//         console.log("sade");
+//     }
+// }
+
+
+
+
+
+// let numbers = [1, 3, 55, 6, 33];
+
+// let result = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (result[i] >= 1 && result[i] <= 20) {
+//         result *= numbers[i];
+//     }
+// }
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
